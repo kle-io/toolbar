87f9a08bba821e539e37e302d90483b91108d6e1
Joshua Lee <joshua_lee@college.harvard.edu>
2019-12-14 20:55:27 -0800

    updated routing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 const db = require('./../database/models/');
+const cors = require('cors');
 
 const app = express();
 
 app.use('/', express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.get('/api/toolbar/songs', (req, res) => {
   db.Song.findAll({}).then((data) => res.send(data));
